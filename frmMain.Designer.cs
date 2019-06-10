2827381a5c536e94b3f6426c84c1e393a9e52f81
Eman <Sammut>
2019-06-10 19:35:14 +0200

    Add project files.

---
+++ frmMain.Designer.cs
@@ -0,0 +1,54 @@
+﻿namespace _8_Ball_Pool___Aim_Hack
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(739, 412);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "8 Ball Pool - Aim Hack";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.FrmMain_MouseDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace _8_Ball_Pool___Aim_Hack
+{
+    public partial class frmMain : Form
+    {
+        //1.0 - Pot Locations
+        Point[] pots = new Point[] { new Point(42, 46), new Point(370, 40), new Point(696, 46), new Point(42, 367), new Point(370, 376), new Point(696, 369) };
+
+        Point mousePosition = new Point();
+        Pen pen = new Pen(Color.Red, 1);
+        Graphics g;
+        GlobalKeyboardHook gHook;
+
+        bool transparent = false;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            g = this.CreateGraphics();
+        }
+
+        protected override void OnMouseMove(MouseEventArgs mouseEv)
+        {
+            mousePosition = mouseEv.Location;
+        }
+
+        private void FrmMain_MouseDown(object sender, MouseEventArgs e)
+        {
+            g.Clear(Color.White);
+            foreach (Point p in pots)
+            {
+                g.DrawLine(pen, mousePosition, p);
+            }
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            gHook = new GlobalKeyboardHook();
+            gHook.KeyDown += new KeyEventHandler(gHook_KeyDown);
+            foreach (Keys key in Enum.GetValues(typeof(Keys)))
+                gHook.HookedKeys.Add(key);
+
+            gHook.hook();
+        }
+
+        private void gHook_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == (char)Keys.LShiftKey)
+            {
+                g.Clear(Color.White);
+            }
+
+            if (e.KeyValue == (char)Keys.LControlKey)
+            {
+                if (!transparent)
+                {
+                    this.TransparencyKey = Color.White;
+                    this.Opacity = 1;
+                } else
+                {
+                    this.TransparencyKey = Color.Black;
+                    this.Opacity = 0.3;
+                }
+                transparent = !transparent;
+            }
+        }
+    }
+}
